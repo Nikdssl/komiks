c44a449a76fbfb3a4b623f79343a0c78d60ed90c
Nikdssl <Nikdssl@yandex.ru>
2021-01-05 18:43:14 +0300

    добавлены якоря и изменение текста на кнопках

---
+++ scripts.js
@@ -0,0 +1,62 @@
+
+    document.querySelector('.button_1').onclick = function() {
+        if(this.innerHTML == "Показать еще")
+        {
+          this.innerHTML = "Свернуть";
+          return;
+        }
+        else this.innerHTML = "Показать еще";
+      }
+
+      document.querySelector('.button_2').onclick = function() {
+        if(this.innerHTML == "Показать еще")
+        {
+          this.innerHTML = "Свернуть";
+          return;
+        }
+        else this.innerHTML = "Показать еще";
+      }
+
+      document.querySelector('.button_2').onclick = function() {
+        if(this.innerHTML == "Показать еще")
+        {
+          this.innerHTML = "Свернуть";
+          return;
+        }
+        else this.innerHTML = "Показать еще";
+      }
+
+      document.querySelector('.button_3').onclick = function() {
+        if(this.innerHTML == "Читать все")
+        {
+          this.innerHTML = "Свернуть";
+          return;
+        }
+        else this.innerHTML = "Читать все";
+      }
+
+      document.querySelector('.button_4').onclick = function() {
+        if(this.innerHTML == "Читать все")
+        {
+          this.innerHTML = "Свернуть";
+          return;
+        }
+        else this.innerHTML = "Читать все";
+      }
+
+     // Найти все ссылки начинающиеся на #
+     const anchors = document.querySelectorAll('a[href^="#"]')
+
+// Цикл по всем ссылкам
+for(let anchor of anchors) {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault() 
+    // Атрибут href у ссылки, если его нет то перейти к body (наверх не плавно)
+    const goto = anchor.hasAttribute('href') ? anchor.getAttribute('href') : 'body'
+    // Плавная прокрутка до элемента с id = href у ссылки
+    document.querySelector(goto).scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    })
+  })
+};
